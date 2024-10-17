d5c6153d42da442f382968b8b005e61aa51fb983
Kishore <kishore07kzr@gmail.com>
2024-10-17 20:02:26 +0530

    Added store funcitonality and an additional helper function to refresh the UI elements in main js file

--- src/main.js
+++ src/main.js
@@ -1,26 +1,36 @@
 import "./styles.css";
-
 import requestVC from "./utils/credential";
 import loginWithIdentity from "./utils/login";
+import { getState } from "./utils/store";
 
-let userPrincipal;
+let state = getState();
 const loginBtn = document.getElementById("login");
 const rpBtn = document.getElementById("rp");
 const princText = document.getElementById("princ");
+const tokenText = document.getElementById("token");
+let decodedToken;
+
+
+const updateUI = () => {
+  state = getState();
+  princText.innerText = state.userPrincipal;
+  tokenText.innerHTML = state.verificationState;
+}
+
+
+document.addEventListener("DOMContentLoaded", () => updateUI());
 
 // Internet Identity login
 loginBtn.addEventListener("click", async() =>  
   {
-    userPrincipal = await loginWithIdentity();
-    princText.innerText = "User Principal: " + userPrincipal;
-    console.log("User Principal: ", userPrincipal);
+    await loginWithIdentity();
+    updateUI();
   })
 
 // Request credeintial
-rpBtn.addEventListener("click", () => {
-  requestVC(
-    userPrincipal,
-    "ICP101",
-    "Jonathan"
-  );
+rpBtn.addEventListener("click", async() => {
+  await requestVC(state.userPrincipal, "ICP101", "Jonathan");
+  console.log(getState());
+  updateUI();
 });
+
