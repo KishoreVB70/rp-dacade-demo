d64eebc2221c2857f8850f3f2af31b2e3703a910
Kishore <kishore07kzr@gmail.com>
2024-10-17 20:01:21 +0530

    Modified credential function to decode the response and further decode it into II response and issuer response and store it in the central store

--- src/utils/credential.js
+++ src/utils/credential.js
@@ -1,31 +1,47 @@
 import { requestVerifiablePresentation } from "@dfinity/verifiable-credentials/request-verifiable-presentation";
 import { Principal } from "@dfinity/principal";
+import { jwtDecode } from "jwt-decode";
+import { updateState } from "./store";
 
 
 // Deployed issuer url with id as query param- https://a4gq6-oaaaa-aaaab-qaa4q-cai.raw.icp0.io/?id=bu5ax-5iaaa-aaaam-qbgcq-cai
-export default function requestVC(userPrincipal, name, recepientName){
-  requestVerifiablePresentation({
-    onSuccess: async (token) => {
-      return token
-    },
-    onError(e) {
-      console.log("Error: ", e);
-    },
-    issuerData: {
-      origin: "https://a4gq6-oaaaa-aaaab-qaa4q-cai.raw.icp0.io/",
-      // Optional
-      canisterId: Principal.fromText("bu5ax-5iaaa-aaaam-qbgcq-cai"),
-    },
-    credentialData: {
-      credentialSpec: {
-        credentialType: `Verified ${name} completion on Dacade`,
-        //Optional
-        arguments: {
-          name: recepientName,
-        },
+export default function requestVC(userPrincipal, name, recepientName) {
+  return new Promise((resolve, reject) => {
+    requestVerifiablePresentation({
+      onSuccess: async (token) => {
+        try {
+          let decodedToken = jwtDecode(String(token.Ok));
+          let decodedIIToken = jwtDecode(decodedToken.vp?.verifiableCredential[0]);
+          let decodedIssuerToken = jwtDecode(decodedToken.vp?.verifiableCredential[1]);
+          updateState({ 
+            decodedToken: decodedToken,
+            decodedIIToken: decodedIIToken,
+            decodedIssuerToken: decodedIssuerToken,
+            verificationState: "User verified"
+          });
+          resolve(decodedToken);  // Resolve the promise with the decoded token
+        } catch (error) {
+          reject(error);  // Handle any error during token decoding or state update
+        }
       },
-      credentialSubject: Principal.fromText(userPrincipal),
-    },
-    identityProvider: new URL("https://identity.ic0.app"),
+      onError(e) {
+        console.log("Error: ", e);
+        reject(e);  // Reject the promise if an error occurs
+      },
+      issuerData: {
+        origin: "https://a4gq6-oaaaa-aaaab-qaa4q-cai.raw.icp0.io/",
+        canisterId: Principal.fromText("bu5ax-5iaaa-aaaam-qbgcq-cai"),
+      },
+      credentialData: {
+        credentialSpec: {
+          credentialType: `Verified ${name} completion on Dacade`,
+          arguments: {
+            name: recepientName,
+          },
+        },
+        credentialSubject: Principal.fromText(userPrincipal),
+      },
+      identityProvider: new URL("https://identity.ic0.app"),
+    });
   });
 }
