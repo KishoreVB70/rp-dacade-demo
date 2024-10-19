907b5996d71ff783620d67402bd131d699d50c53
Kishore <kishore07kzr@gmail.com>
2024-10-19 16:02:43 +0530

    refactor: migrate login.js to TypeScript(login.ts)

--- src/utils/login.ts
+++ src/utils/login.ts
@@ -1,12 +1,12 @@
 import { AuthClient } from '@dfinity/auth-client';
 import { updateState } from './store';
 
-export default async function loginWithloginWithIdentity(){
-    const authClient = await AuthClient.create();
-    const isAuthenticated = await authClient.isAuthenticated();
+export default async function loginWithloginWithIdentity(): Promise<String>{
+    const authClient: AuthClient = await AuthClient.create();
+    const isAuthenticated: boolean = await authClient.isAuthenticated();
     
     if(!isAuthenticated) {
-        await new Promise((resolve) => {
+        await new Promise<void>((resolve) => {
             authClient.login({
                 // For NFID -> https://nfid.one/authenticate
                 identityProvider: "https://identity.ic0.app/",
@@ -14,8 +14,9 @@ export default async function loginWithloginWithIdentity(){
             });
         });
     }
+    
     const identity = authClient.getIdentity();
-    const principal = identity.getPrincipal().toString();
+    const principal: string = identity.getPrincipal().toString();
     updateState({userPrincipal: principal});
     return principal;
 }
