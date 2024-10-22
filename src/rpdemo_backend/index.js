5b3c7ae0335cc90eecfac241b40ebdf8c917cf56
Kishore <kishore07kzr@gmail.com>
2024-10-22 14:00:37 +0530

    feat: added function to create rpBackendActor in the login file

--- src/rpdemo_backend/index.js
+++ src/rpdemo_backend/index.js
@@ -6,11 +6,9 @@ export { idlFactory } from "./rpdemo_backend.did.js";
 
 /* CANISTER_ID is replaced by webpack based on node environment
  * Note: canister environment variable will be standardized as
- * process.env.CANISTER_ID_<CANISTER_NAME_UPPERCASE>
  * beginning in dfx 0.15.0
  */
-export const canisterId =
-  process.env.CANISTER_ID_RPDEMO_BACKEND;
+export const canisterId = "c5kvi-uuaaa-aaaaa-qaaia-cai";
 
 export const createActor = (canisterId, options = {}) => {
   const agent = options.agent || new HttpAgent({ ...options.agentOptions });
@@ -19,16 +17,6 @@ export const createActor = (canisterId, options = {}) => {
     console.warn(
       "Detected both agent and agentOptions passed to createActor. Ignoring agentOptions and proceeding with the provided agent."
     );
-  }
-
-  // Fetch root key for certificate validation during development
-  if (process.env.DFX_NETWORK !== "ic") {
-    agent.fetchRootKey().catch((err) => {
-      console.warn(
-        "Unable to fetch root key. Check to ensure that your local replica is running"
-      );
-      console.error(err);
-    });
   }
 
   // Creates an actor with using the candid interface and the HttpAgent
--- src/utils/login.ts
+++ src/utils/login.ts
@@ -1,10 +1,11 @@
 import { AuthClient } from '@dfinity/auth-client';
 import { updateState } from './store';
+import { idlFactory, canisterId} from '../rpdemo_backend';
+import { HttpAgent, Actor, Identity} from '@dfinity/agent';
 
-export default async function loginWithloginWithIdentity(): Promise<String>{
+async function authenticateUser(): Promise<Identity> {
     const authClient: AuthClient = await AuthClient.create();
     const isAuthenticated: boolean = await authClient.isAuthenticated();
-    
     if(!isAuthenticated) {
         await new Promise<void>((resolve) => {
             authClient.login({
@@ -14,8 +15,23 @@ export default async function loginWithloginWithIdentity(): Promise<String>{
             });
         });
     }
-    
-    const identity = authClient.getIdentity();
+    return authClient.getIdentity();
+}
+
+export async function getRpBackendActor(): Promise<Actor> {
+    const identity = authenticateUser();
+    const rpBackendActor = Actor.createActor(idlFactory, {
+        agent: new HttpAgent({
+            identity,
+        }),
+        canisterId,
+    });
+    console.log(rpBackendActor);
+    return rpBackendActor;
+};
+
+export default async function loginWithloginWithIdentity(): Promise<String>{
+    const identity = authenticateUser();
     const principal: string = identity.getPrincipal().toString();
     updateState({userPrincipal: principal});
     return principal;
