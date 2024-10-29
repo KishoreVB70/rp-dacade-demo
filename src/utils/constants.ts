c007a2109155e836cddf1c7db6a47ea02d7f375e
Kishore <kishore07kzr@gmail.com>
2024-10-29 16:05:27 +0530

    fix: fixed the issuer canister url in the mainnet to antoher version

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 const ii_url_mainnet: string = "https://identity.ic0.app";
 
 const issuer_canister_id_mainnet: string = "bu5ax-5iaaa-aaaam-qbgcq-cai";
-const issuer_url_mainnet:string = "https://bu5ax-5iaaa-aaaam-qbgcq-cai.raw.icp0.io/";
+const issuer_url_mainnet:string = "https://a4gq6-oaaaa-aaaab-qaa4q-cai.raw.icp0.io/";
 
 const ii_url_experimental_mainnet: string = "https://identity.internetcomputer.org/";
 
@@ -14,3 +14,7 @@ const issuer_url_local: string = "http://127.0.0.1:4943/?canisterId=bw4dl-smaaa-
 export const ii_url: string = ii_url_experimental_mainnet;
 export const issuer_canister_id: string = issuer_canister_id_mainnet;
 export const issuer_url: string = issuer_url_mainnet;
+//https://identity.internetcomputer.org/#authorize
+//https://a4gq6-oaaaa-aaaab-qaa4q-cai.raw.icp0.io
+
+// Principal backend - ysixd-7i5eo-cshao-cbnqu-iur5b-ihv32-tl6tr-bar6f-dmvyo-ffvry-yae
