4c072662ba3fad45131bbc9a798153f695bbf591
Kishore <kishore07kzr@gmail.com>
2024-11-15 08:17:13 +0530

    refactor: add pop up window for login

--- src/utils/login.ts
+++ src/utils/login.ts
@@ -2,6 +2,21 @@ import { AuthClient } from '@dfinity/auth-client';
 import { updateState } from './store';
 import { Identity } from '@dfinity/agent';
 import { ii_url } from './constants';
+const popupCenter = (): string | undefined => {
+    const AUTH_POPUP_WIDTH = 576;
+    const AUTH_POPUP_HEIGHT = 625;
+  
+    if (typeof window === "undefined" || !window.top) {
+      return undefined;
+    }
+  
+    const { innerWidth, innerHeight, screenX, screenY } = window;
+  
+    const y = innerHeight / 2 + screenY - AUTH_POPUP_HEIGHT / 2;
+    const x = innerWidth / 2 + screenX - AUTH_POPUP_WIDTH / 2;
+  
+    return `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width=${AUTH_POPUP_WIDTH}, height=${AUTH_POPUP_HEIGHT}, top=${y}, left=${x}`;
+  };
 
 async function authenticateUser(): Promise<Identity> {
     const authClient: AuthClient = await AuthClient.create();
@@ -11,6 +26,7 @@ async function authenticateUser(): Promise<Identity> {
             authClient.login({
                 identityProvider: ii_url,
                 onSuccess: resolve,
+                windowOpenerFeatures: popupCenter()
             });
         });
     }
