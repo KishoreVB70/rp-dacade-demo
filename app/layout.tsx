47f48683aa9a8e132da346b4301bc2b2f3ddb85a
Kishore <kishore07kzr@gmail.com>
2024-11-19 17:40:32 +0530

    feat: remove boiler plate and add essential code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,22 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Manrope } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-manrope",
+  display: "swap",
+});
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -24,11 +34,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className={manrope.variable}>
+      <body className="font-sans antialiased">
+        <div className="min-h-screen bg-muted flex flex-col">
+          <div className="flex flex-col bg-background w-full max-w-5xl mx-auto lg:my-8 lg:border border-border lg:drop-shadow-sm flex-grow">
+            {children}
+            <Footer />
+          </div>
+        </div>
       </body>
     </html>
   );
