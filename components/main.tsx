87981f6bb64ebb938e0ac45b489ad64418000d9f
Kishore <kishore07kzr@gmail.com>
2024-11-20 09:04:07 +0530

    feat: update main component to contain heading and button

--- components/main.tsx
+++ components/main.tsx
@@ -1,8 +1,25 @@
+"use client"
 import React from 'react'
+import { Button } from './ui/button'
+import { useAuth } from '@/lib/context/AuthContext'
 
 const Main = () => {
+  const { principal } = useAuth();
   return (
-    <div>Main</div>
+    <div className='flex flex-grow' >
+      <div className='flex flex-col flex-grow justify-center items-center'>
+        <h2 className="text-3xl font-bold">
+          Welcome to the Issuer demo App! Obtain your <br />
+          <span className='pl-8'>
+            verifiable credential and test
+            <a href="#" className="pl-2 text-primary hover:underline">Badgegate</a>.
+          </span>
+        </h2>
+        {principal
+          ?(<Button className='mt-10' >Get credential</Button>)
+          :(<Button className='mt-10' variant='disabled'>Get credential</Button>)}
+      </div>
+    </div>
   )
 }
 
