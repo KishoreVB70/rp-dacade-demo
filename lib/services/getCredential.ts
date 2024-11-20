69760eac5292f08ec6121c7095c2f805e49d4115
Kishore <kishore07kzr@gmail.com>
2024-11-20 12:20:36 +0530

    feat: add getCredential function to call the backend

---
+++ lib/services/getCredential.ts
@@ -0,0 +1,26 @@
+import {Actor, HttpAgent, Identity} from "@dfinity/agent";
+import {idlFactory as issuerIDL} from "@/lib/issuer";
+import { issuer_canister_id } from "../constants";
+
+
+export const getCredential = async(identity: Identity | null) => {
+    try {
+        if(identity == null) {
+          return;
+        }
+    
+        const agent = await HttpAgent.create({ host: 'https://ic0.app', identity });
+        console.log(agent);
+    
+        const actor = Actor.createActor(issuerIDL, {
+          agent,
+          canisterId: issuer_canister_id,
+        });
+    
+        let result = await actor.add_course_completion("typescript-smart-contract-101");    
+        console.log(result);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
