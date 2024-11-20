a84dda67ef22611784e788b395e2f3d6864277a0
Kishore <kishore07kzr@gmail.com>
2024-11-20 13:48:16 +0530

    feat: add navbar custom avatar to disconnect the app

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,10 +5,14 @@ import React, { useState} from 'react'
 import { Button } from "@/components/ui/button"
 import { useAuth } from '@/lib/context/AuthContext'
 import useICPAuth from '@/hooks/useICPAuth'
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import Avatar from 'boring-avatars'
+import { UserMenu } from '@/components/ui/user-menu'
 
 export default function Navbar() {
   const {loginWithInternetIdentity, logout, isLoading} = useICPAuth();
   const { principal } = useAuth();
+  const avatarProp = principal ?? "Default";
 
   const handleSignInClick = async() => {
     await loginWithInternetIdentity();
@@ -23,17 +27,27 @@ export default function Navbar() {
       <p className='font-extrabold text-xl' >Issuer Demo App</p>
       <div className="sm:w-1/3 flex justify-end">
         <div className="h-10 flex items-center">
-            {principal
-                ?(
-                    <Button variant="outline" onClick={handleDisconnectWallet}>
-                        Sign Out
-                    </Button>
-                )
-                :(
-                    <Button variant="outline" onClick={handleSignInClick}>
-                        Sign In
-                    </Button>
-            )}
+          {isLoading
+            ?<div className="animate-spin duration-500 rounded-full h-10 mr-6 py-2 w-6 border-t-2 border-gray-500"></div>
+            :principal
+              ?(
+                <Popover>
+                <PopoverTrigger asChild>
+                  <Button variant="ghost" className="mr-4 h-10 w-10 p-0 rounded-full">
+                  <Avatar name={avatarProp} size={40} />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-80 p-0" align="end">
+                  <UserMenu handleDisconnectWallet={handleDisconnectWallet} />
+                </PopoverContent>
+              </Popover>
+              )
+              :(
+                <Button variant="outline" onClick={handleSignInClick}>
+                    Sign In
+                </Button>
+              )
+          }
         </div>
       </div>
     </nav>
