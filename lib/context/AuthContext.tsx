665df6eba80ecb11aedf226d201a2c2391a32d8e
Kishore <kishore07kzr@gmail.com>
2024-11-20 12:21:03 +0530

    feat: update authContext to contain the identity

--- lib/context/AuthContext.tsx
+++ lib/context/AuthContext.tsx
@@ -1,18 +1,22 @@
 "use client";
 import React, { createContext, useContext, useState, ReactNode } from 'react';
+import { Identity } from "@dfinity/agent";
 
 interface AuthContextType {
   principal: string | null;
   setPrincipal: (principal: string | null) => void;
+  setIdentity: (identity: Identity | null) => void;
+  identity: Identity | null;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [principal, setPrincipal] = useState<string | null>(null);
+  const [identity, setIdentity] = useState<Identity | null>(null);
 
   return (
-    <AuthContext.Provider value={{ principal, setPrincipal }}>
+    <AuthContext.Provider value={{ principal, setPrincipal, identity, setIdentity }}>
       {children}
     </AuthContext.Provider>
   );
