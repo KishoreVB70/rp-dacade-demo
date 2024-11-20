cb6091320398341d809b6c3c72689b00bd53e0be
Kishore <kishore07kzr@gmail.com>
2024-11-20 12:24:46 +0530

    style: add top border to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="px-6 py-5">
+    <footer className="px-6 py-5 border-t border-border">
       <p className="text-xs">© Issuer Demo App 2024</p>
     </footer>
   )
